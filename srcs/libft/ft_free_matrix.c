581a9f62bcc99f7c47569a9351e8d59990a56865
MVPee <mariusvanpee@hotmail.be>
2023-12-14 17:15:45 +0100

    prototype change

--- srcs/libft/ft_free_matrix.c
+++ srcs/libft/ft_free_matrix.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_free_split.c                                    :+:      :+:    :+:   */
+/*   ft_free_matrix.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:23:11 by mvpee             #+#    #+#             */
-/*   Updated: 2023/12/07 14:51:04 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/12/14 17:15:20 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
-void	ft_free_split(unsigned int count, ...)
+void	ft_free_matrix(unsigned int count, ...)
 {
 	va_list	args;
 	void	***array;
